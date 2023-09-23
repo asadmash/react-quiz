a0bd49c8cdd93d8ba0f1554d1d2da91f63a8be03
smMashuq <smmasukkhan@gmail.com>
2023-09-23 07:21:37 +0600

    home page complete

---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,15 @@
+
+import Video from '../Videos'
+
+export default function Home() {
+    return (
+        <>
+         < Video/>
+       < Video/>
+       < Video/>
+       < Video/>
+       < Video/>
+       < Video/>
+        </>
+  )
+}
