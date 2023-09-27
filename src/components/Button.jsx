cca9250a2263ab3201503ae90a77a9bd58cd0736
smMashuq <smmasukkhan@gmail.com>
2023-09-27 08:29:29 +0600

    Quiz component refactored

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,5 @@
 import classes from "../styles/Button.module.css";
 // eslint-disable-next-line react/prop-types
-export default function Button({ children }) {
-  return (
-    <div className={classes.button}>
-      <span>{children}</span>
-    </div>
-  );
+export default function Button({ className, children }) {
+  return <div className={`${classes.button} ${className}`}>{children}</div>;
 }
