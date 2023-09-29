78ceefdecd0059cedb71195a5aec4cace7724cd1
smMashuq <smmasukkhan@gmail.com>
2023-09-29 10:22:25 +0600

    lesson-8 complete

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,19 +4,22 @@ import Home from "./pages/Home";
 import Signup from "./pages/Signup";
 import Quiz from "./pages/Quiz";
 import Result from "./pages/Result";
+import { AuthProvider } from "../contexts/AuthContext";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <Router>
-      <Layout>
-        <Routes>
-          <Route exact path="/" Component={Home} />
-          <Route exact path="/signup" Component={Signup} />
-          <Route exact path="/quiz" Component={Quiz} />
-          <Route exact path="/result" Component={Result} />
-        </Routes>
-      </Layout>
+      <AuthProvider>
+        <Layout>
+          <Routes>
+            <Route exact path="/" Component={Home} />
+            <Route exact path="/signup" Component={Signup} />
+            <Route exact path="/quiz" Component={Quiz} />
+            <Route exact path="/result" Component={Result} />
+          </Routes>
+        </Layout>
+      </AuthProvider>
     </Router>
   );
 }
